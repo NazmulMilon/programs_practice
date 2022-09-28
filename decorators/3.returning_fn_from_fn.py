f348d6396c3da860b8ff2406a447eaea7627d2cf
nazmulmilon <milonsarker73@gmail.com>
2022-09-28 09:52:59 +0600

    returning functions from functions

---
+++ decorators/3.returning_fn_from_fn.py
@@ -0,0 +1,23 @@
+
+
+def parent(num):
+    def first_child():
+        return "Hi, I am Milon"
+
+    def second_child():
+        return "Hi, I am Nahid"
+
+    if num == 1:
+        return first_child()
+    else:
+        return second_child()
+
+    # without parenthesis it will print the refernce of the first_child and second_child function.
+    # if num == 1:
+    #     return first_child
+    # else:
+    #     return second_child
+
+
+print(parent(1))
+print(parent(1))
