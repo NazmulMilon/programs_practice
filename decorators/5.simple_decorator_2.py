fbf00cd5810ee65f9e229e6c1188018065986066
nazmulmilon <milonsarker73@gmail.com>
2022-09-28 11:00:40 +0600

    simple decorator example-2

---
+++ decorators/5.simple_decorator_2.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+
+def not_during_the_night(func):
+    def wrapper():
+        if 7<=datetime.now().hour < 22:
+            func()
+        else:
+            pass    # neighbours are asleep
+    return wrapper
+
+
+def say_something():
+    print("something different")
+
+
+say_something = not_during_the_night(say_something)
+
+print(say_something())
