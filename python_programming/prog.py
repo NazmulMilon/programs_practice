fd82a6f9e0a6642cdd17e02dbb7853a3a2ad661d
nazmulmilon <milonsarker73@gmail.com>
2022-10-31 16:01:27 +0600

    prog.py file

---
+++ python_programming/prog.py
@@ -0,0 +1,21 @@
+# rows, cols = (5, 5)
+#
+# arr = [[0 for i in range(cols)] for j in range(rows)]
+# print(arr)
+#
+# for row in arr:
+#     print(row)
+
+car = {'123': [[1, 1, 1], [2, 2, 2], [3, 3, 3]], '456': [[4, 4, 4], [5, 5, 5], [6, 6, 6]]}
+
+print(car['123'])
+
+''''
+
+rows, cols = (3, 3)
+
+for i in range(1,rows+1):
+    for j in range(1,cols+1):
+        print(f"({i},{j})", end=" ")
+    print()
+'''
